adb56442a3d86def7d355a472eec76ac4ee1a2ad
Ladislav Prskavec <ladislav@prskavec.net>
2013-02-15 19:58:45 +0100

    Add support for PaaS Openshift

--- app.js
+++ app.js
@@ -99,6 +99,7 @@ fs.exists('./plugins/index.js', function(exists) {
   }
 });
 
-var port = process.env.PORT || config.server.port;
-server.listen(port);
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || config.server.port;
+var ip = process.env.OPENSHIFT_INTERNAL_IP || config.server.ip;
+server.listen(port, ip);
 console.log("Express server listening on port %d in %s mode", port, app.settings.env);
